cab3460f2215a442846b0250533bdc96e231fb82
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-02-10 08:45:09 +0530

    08/02/2024 Leetcode problem solution updated

---
+++ PerfectSquare.java
@@ -0,0 +1,33 @@
+public class PerfectSqyare {
+  public static void main(String[] args) {
+    System.out.println(numSquares(121));
+  }
+
+  public static int numSquares(int n) {
+    if (isPerfectSquare(n)) {
+      return 1;
+    }
+
+    for (int i = 1; i * i <= n; i++) {
+      if (isPerfectSquare(n - i * i)) {
+        return 2;
+      }
+    }
+
+    while (n % 4 == 0) {
+      n /= 4;
+    }
+    if (n % 8 == 7) {
+      return 4;
+    }
+
+    return 3;
+  }
+
+  private static boolean isPerfectSquare(int n) {
+
+    int sqrt = (int) Math.sqrt(n);
+    return sqrt * sqrt == n;
+
+  }
+}
